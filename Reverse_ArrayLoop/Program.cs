9d4bddc7463d297102a45399c7fe35bef17d1eda
admin <admin@CHETUIWK1901>
2022-05-10 17:08:50 +0530

    Add project files.

---
+++ Reverse_ArrayLoop/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Reverse_ArrayLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            for (int i = arr.Length-1; i >= 0; i--)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            
+        }
+    }
+}
